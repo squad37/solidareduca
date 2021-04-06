f63dd1860a9363f84be52ca62359a2490c835cb2
brenoRM <breno.moura@ipea.gov.br>
2021-04-05 21:03:35 -0300

    registrando handlebar parapoder utilizar xif nohandlebars

--- public/js/app.js
+++ public/js/app.js
@@ -2,6 +2,33 @@
  * Created by brenokx on 26/03/2021.
  */
 window.addEventListener('load', () => {
+
+    Handlebars.registerHelper("x", function(expression, options) {
+        var result;
+
+        // you can change the context, or merge it with options.data, options.hash
+        var context = this;
+
+        // yup, i use 'with' here to expose the context's properties as block variables
+        // you don't need to do {{x 'this.age + 2'}}
+        // but you can also do {{x 'age + 2'}}
+        // HOWEVER including an UNINITIALIZED var in a expression will return undefined as the result.
+        with(context) {
+            result = (function() {
+                try {
+                    return eval(expression);
+                } catch (e) {
+                    console.warn('•Expression: {{x \'' + expression + '\'}}\n•JS-Error: ', e, '\n•Context: ', context);
+                }
+            }).call(context); // to make eval's lexical this=context
+        }
+        return result;
+    });
+
+    Handlebars.registerHelper("xif", function(expression, options) {
+        return Handlebars.helpers["x"].apply(this, [expression, options]) ? options.fn(this) : options.inverse(this);
+    });
+
     const el = $('#app');
 
     // Compile Handlebar Templates
@@ -504,6 +531,7 @@ window.addEventListener('load', () => {
 
     // Display Error Banner
     const showError = (error) => {
+        console.log(error);
         console.log(error.response);
         console.log(error.response.hasOwnProperty('data'));
         if (error.response.hasOwnProperty('data')) {
